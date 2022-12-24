e9376feb36dd4a3a7daa08036d8771c57e628b86
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 05:29:44 +0100

    commit zero

---
+++ A Universal Time Script pastebin roblox A Universal Time Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/KnRVB0Dm", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Interesting script, easy to understand.It features Farm zombies, farm Dio, Anty AFK and more!
+						
+]]
